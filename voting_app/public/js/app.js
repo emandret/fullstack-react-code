943f82e4d05b5538b240b9a34f4ce42896296784
Edwy Mandret <emandret@student.42.fr>
2020-06-20 22:47:01 +0200

    cleanup

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,72 +1,93 @@
 class Product extends React.Component {
 
-    handleUpVote = () => {
-        this.props.onVote(this.props.id)
+    constructor(props) {
+        // Always call this first
+        super(props);
+
+        // Custom method bindings here for the onClick event
+        this.handleUpVote = this.handleUpVote.bind(this);
+    }
+
+    handleUpVote() {
+        this.props.onVote(this.props.id);
     }
 
     render() {
+        const {
+            title,
+            description,
+            url,
+            votes,
+            submitterAvatarUrl,
+            productImageUrl,
+        } = this.props;
+
         return (
-            <div className='item'>
-                <div className='image'>
-                    <img src={this.props.productImageUrl}/>
+            <div className="item">
+                <div className="image">
+                    <img src={productImageUrl} />
                 </div>
-                <div className='middle aligned content'>
-                    <div className='header'>
-                        <a onClick={this.handleUpVote}>
-                            <i className='large caret up icon'/>
+                <div className="middle aligned content">
+                    <div className="header">
+                        <a>
+                            <i onClick={this.handleUpVote} className="large caret up icon" />
                         </a>
-                        {this.props.votes}
+                        {votes}
                     </div>
-                    <div className='description'>
-                        <a href={this.props.url}>{this.props.title}</a>
-                        <p>{this.props.description}</p>
+                    <div className="description">
+                        <a href={url}>{title}</a>
+                        <p>{description}</p>
                     </div>
-                    <div className='extra'>
-                        <span>Submitted by:</span>
-                        <img className='ui avatar image' src={this.props.submitterAvatarUrl}/>
+                    <div className="extra">
+                        <span>Submitted by: </span>
+                        <img className="ui avatar image" src={submitterAvatarUrl} />
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
 class ProductList extends React.Component {
 
-    componentDidMount() {
-        this.setState({products: Seed.products})
+    constructor(props) {
+        super(props);
+
+        // Set the state
+        this.state = { products: [] };
+
+        // Custom method bindings here for the onClick event
+        this.handleProductUpVote = this.handleProductUpVote.bind(this);
     }
 
-    state = {
-            products: [],
-        };
+    componentDidMount() {
+        this.setState({ products: Seed.products });
+    }
 
-    handleProductUpVote = (productID) => {
-        console.log(productID + 'was upvoted.');
-
+    handleProductUpVote(productId) {
         const nextProducts = this.state.products.map((product) => {
-            if (product.id === productID) {
+            if (product.id === productId) {
                 return Object.assign({}, product, {
-                    votes: product.votes + 1
-                })
+                    votes: product.votes + 1,
+                });
             } else {
-                return product
+                return product;
             }
         });
 
-        this.setState({
-            products: nextProducts
-        });
-
-    };
+        // Enqueues changes to the component state and tells React that this component
+        // and its children need to be re-rendered
+        this.setState({ products: nextProducts });
+    }
 
     render() {
-        const products = this.state.products.sort((a, b) => (
+        this.state.products.sort((a, b) => (
             b.votes - a.votes
         ));
-        const productComponents = products.map((product) => (
+
+        const productComponents = this.state.products.map((product) => (
             <Product
-                key={'product-' + product.id}
+                key={product.id}
                 id={product.id}
                 title={product.title}
                 description={product.description}
@@ -79,15 +100,14 @@ class ProductList extends React.Component {
         ));
 
         return (
-            <div className='ui stackable items'>
+            <div className="ui unstackable items">
                 {productComponents}
             </div>
         );
     }
 }
 
-
 ReactDOM.render(
-    <ProductList/>,
-    document.getElementById('content')
+    <ProductList />,
+    document.getElementById("content"),
 );
